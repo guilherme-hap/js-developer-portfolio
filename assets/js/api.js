2dde2cfa9f90ff4a58f2abcbfeba58e68d22538b
Guilherme Henrique <140356085+guilherme-hap@users.noreply.github.com>
2023-07-31 01:30:52 +0000

    Testando o usuário

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://github.dev/guilherme-hap/js-developer-portfolio/blob/main/data/profile.json';
+    const url = 'https://github.com/guilherme-hap/js-developer-portfolio/blob/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
